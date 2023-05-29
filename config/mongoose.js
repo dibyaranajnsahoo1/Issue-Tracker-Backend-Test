7a3cf22607d17a99eb1459e044b7813d2e9568f8
dibyaranjan1 <dibyaranjansahoo955@gmail.com>
2023-05-29 11:39:07 +0530

    Issue tracker project

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://dibya:dibya@cluster0.p2zyhun.mongodb.net/?retryWrites=true&w=majority';
+// mongoose.connect('url') 
+mongoose.connect(url);
+const db=mongoose.connection;
+
+//If any Error then Getting this Line
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+
+
+db.once('open',()=>{
+    console.log("Connected to Database :: MongoDB ")
+});
+
+module.exports=db;  //Exports db
